a2866272325119f1c7b686682bf41ad83e98a7f2
Hyunwoo <hyunwoo2546@naver.com>
2022-10-31 22:29:26 +0900

    댓글 기본적인 CRUD 틀 구성.

---
+++ TwoLineProject/src/main/java/com/hyunwoo/mapper/ReplyMapper.java
@@ -0,0 +1,24 @@
+package com.hyunwoo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.hyunwoo.domain.Criteria;
+import com.hyunwoo.domain.ReplyVO;
+
+public interface ReplyMapper {
+	
+	public int insert(ReplyVO vo);
+	
+	public ReplyVO read(Long bno);
+	
+	public int delete (int rno);
+	
+	public int update (ReplyVO reply);
+	
+	public List<ReplyVO> getListWithPaging(
+			@Param("cri") Criteria cri,
+			@Param("bno") Long bno);
+	
+}
